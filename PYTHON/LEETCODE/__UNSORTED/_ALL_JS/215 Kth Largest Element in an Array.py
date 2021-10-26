702b1afcbd05336c57d4bbf06cf15228b997adf0
bryan <bryan.guner@gmail.com>
2021-10-26 16:56:09 -0400

    hi

--- PYTHON/LEETCODE/__UNSORTED/_ALL_JS/215 Kth Largest Element in an Array.py
+++ PYTHON/LEETCODE/__UNSORTED/_ALL_JS/215 Kth Largest Element in an Array.py
@@ -6,17 +6,19 @@ class Solution(object):
         :rtype: int
         """
 
-        pivot = random.choice(nums);
+        pivot = random.choice(nums)
         nums1, nums2 = [], []
         for num in nums:
-          if num > pivot:
-            nums1.append(num)
-          elif num < pivot:
-            nums2.append(num)
+            if num > pivot:
+                nums1.append(num)
+            elif num < pivot:
+                nums2.append(num)
 
         if k <= len(nums1):
-          return self.findKthLargest(nums1, k)
-        if k > len(nums) - len(nums2): # draw a graph to visualize it! It's not in the top k assortment, but in the small section
-          return self.findKthLargest(nums2, k - (len(nums) - len(nums2)))
+            return self.findKthLargest(nums1, k)
+        if k > len(nums) - len(
+            nums2
+        ):  # draw a graph to visualize it! It's not in the top k assortment, but in the small section
+            return self.findKthLargest(nums2, k - (len(nums) - len(nums2)))
 
         return pivot
