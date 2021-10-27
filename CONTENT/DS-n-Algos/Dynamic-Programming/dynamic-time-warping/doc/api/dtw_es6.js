909c957997ad0c10f6866a710f7b3753c6779e47
bryan <bryan.guner@gmail.com>
2021-10-27 07:48:34 -0400

    hi

---
+++ CONTENT/DS-n-Algos/Dynamic-Programming/dynamic-time-warping/doc/api/dtw_es6.js
@@ -0,0 +1,16 @@
+## Platforms
+
+- [ Meetup.com ](//meetup.com)
+  > One of the most common events platforms today.Meetup has an
+- [ API ](https://www.meetup.com/meetup_api/)
+  > with various( community - contributed )[ client libraries](https://github.com/meetup/api/blob/master/clients.md).
+- [ Sessionize.com ](//sessionize.com)
+  > A platform for hosting call for speakers / call for papers
+- [ Joind.in ](//joind.in)
+  > Call for papers and feedback platform
+- [ Papercall.io ](//papercall.io)
+  > Call for papers platform
+- [ Attending.io ](//attending.io/)
+  > A great event platform for free events
+- [ Picatic ](//picatic.com)
+  > An easy - to - use event registration platform for free and paid events with a strong API for developers
