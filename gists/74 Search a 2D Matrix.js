0ecec35343ddf8afffb155a0c2cedf23cb1f33bb
Bryan C Guner <66654881+bgoonz@users.noreply.github.com>
2022-01-10 04:51:42 -0500

    Add files via upload

---
+++ gists/74 Search a 2D Matrix.js
@@ -0,0 +1,46 @@
+// Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+// Integers in each row are sorted in ascending from left to right.
+// Integers in each column are sorted in ascending from top to bottom.
+// For example,
+
+// Consider the following matrix:
+
+// [
+//   [1,   4,  7, 11, 15],
+//   [2,   5,  8, 12, 19],
+//   [3,   6,  9, 16, 22],
+//   [10, 13, 14, 17, 24],
+//   [18, 21, 23, 26, 30]
+// ]
+// Given target = 5, return true.
+
+// Given target = 20, return false.
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+
+var searchMatrix = function (matrix, target) {
+  if (matrix === null || matrix.length === 0 || matrix[0].length === 0) {
+    return false;
+  }
+
+  var rows = matrix.length;
+  var cols = matrix[0].length;
+
+  var y = cols - 1;
+
+  for (var x = 0; x < rows; x++) {
+    while (y && target < matrix[x][y]) {
+      y--;
+    }
+    if (matrix[x][y] === target) {
+      return true;
+    }
+  }
+
+  return false;
+};
---
+++ gists/78 Subsets.js
@@ -0,0 +1,73 @@
+// Given a set of distinct integers, nums, return all possible subsets.
+
+// Note: The solution set must not contain duplicate subsets.
+
+// For example,
+// If nums = [1,2,3], a solution is:
+
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1,2,3],
+//   [1,3],
+//   [2,3],
+//   [1,2],
+//   []
+// ]
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+  var result = [];
+  generate(nums, 0, [], result);
+  return result;
+};
+
+var generate = function (nums, index, cur, result) {
+  if (index === nums.length) {
+    result.push(cur.slice());
+    return;
+  }
+
+  generate(nums, index + 1, cur, result);
+  cur.push(nums[index]);
+  generate(nums, index + 1, cur, result);
+  cur.pop();
+};
+
+// second try
+var subsets = function (nums) {
+  var res = [[]];
+
+  function generate(nums, i, cur, res) {
+    for (; i < nums.length; i++) {
+      cur.push(nums[i]);
+      res.push(cur.slice());
+      generate(nums, i + 1, cur, res);
+      cur.pop();
+    }
+  }
+
+  generate(nums, 0, [], res);
+  return res;
+};
+
+// Third solution
+var subsets = function (nums) {
+  var results = [];
+  combine(nums, 0, [], results);
+  return results;
+};
+
+var combine = function (nums, index, partial, results) {
+  if (index === nums.length) {
+    results.push(partial);
+    return;
+  }
+
+  combine(nums, index + 1, partial, results);
+  combine(nums, index + 1, partial.concat([nums[index]]), results);
+};
