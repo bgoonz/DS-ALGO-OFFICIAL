b9418c63d94823b2cdafe7483d5516028fa2851a
Bryan C Guner <bryan.guner@gmail.com>
2022-06-24 07:19:14 +0000

    -update

--- gists/cookieCutter.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    var cookieParts = {};
-    window.top.document.cookie.split("; ").forEach(cookieCutter);
-    function cookieCutter(part) {
-        var split = part.split("=");
-        cookieParts[split[0]] = split[1];
-    }
-})();
